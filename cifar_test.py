dc510c11c046cebf1f4a5d6c22daef1c87fc89f5
abhay mishra <38161531+loginabhay@users.noreply.github.com>
2019-01-08 19:09:03 +0530

    Update cifar_test.py

--- cifar_test.py
+++ cifar_test.py
@@ -3,7 +3,6 @@ from scipy.misc import toimage
 from keras.datasets import cifar10
 import numpy as np
 from keras.models import model_from_json
-from sklearn.metrics import classification_report
 
 def show_imgs(X):
     pyplot.figure(1)
@@ -16,12 +15,8 @@ def show_imgs(X):
             k = k+1
     # show the plot
     pyplot.show()
-model = load_('model.h5')
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-predicted_classes = model.predict(x_test)
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 # mean-std normalization
 mean = np.mean(x_train ,axis=(0 ,1 ,2 ,3))
@@ -44,4 +39,3 @@ indices = np.argmax(model.predict(x_test[:16]) ,1)
 print ([labels[x] for x in indices])
 num_classes = 10
 target_names = ["Classe {}".format(i) for i in range(num_classes)]
-print(classification_report(y_test, predicted_classes, target_names=target_names))
